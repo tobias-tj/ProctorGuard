f1aa328017d3c567c1070704065de7e6fd500a30
tobias-tj <https.tobias.jara.404@gmail.com>
2025-03-26 20:46:44 -0300

    Se ajusta el caso de uso para recuperar examenes

--- src/api/admin/dashboardAdmin.ts
+++ src/api/admin/dashboardAdmin.ts
@@ -2,48 +2,44 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-// export const fetchDashboardData = async () => {
-//   try {
-//     const response = await axios.get(
-//       `http://161.35.53.140/back/api/getAllStudentsCount`
-//     );
-//     return response.data.data;
-//   } catch (error) {
-//     console.error("Error fetching dashboard data:", error);
-//     throw new Error("Failed to fetch dashboard information");
-//   }
-// };
-
-// export const fetchDashboardExamTotalCount = async () => {
-//   try {
-//     const response = await axios.get(
-//       `http://161.35.53.140/back/api/getAllTotalExamCount`
-//     );
-//     return response.data.data;
-//   } catch (error) {
-//     console.error("Error fetching exam dashboard count:", error);
-//     throw new Error("Failed to fetch dashboard information");
-//   }
-// };
-
-export const fetchDashboardExamCleanCount = async () => {
+export const fetchDashboardData = async () => {
   try {
-    const response = await axios.get(`${API_URL}/getExamClean`);
-    return response.data;
+    const response = await axios.get(`${API_URL}/getAllStudentsCount`);
+    return response.data.data;
   } catch (error) {
-    console.error("Error fetching exam total count:", error);
+    console.error("Error fetching dashboard data:", error);
     throw new Error("Failed to fetch dashboard information");
   }
 };
 
-export const fetchDashboardExamIncidentCount = async () => {
+export const fetchDashboardExamTotalCount = async () => {
   try {
-    const response = await axios.get(
-      `${API_URL}/getExamIncident?countOnly=true`
-    );
-    return response.data;
+    const response = await axios.get(`${API_URL}/getAllTotalExamCount`);
+    return response.data.data;
   } catch (error) {
-    console.error("Error fetching exam total count:", error);
+    console.error("Error fetching exam dashboard count:", error);
     throw new Error("Failed to fetch dashboard information");
   }
 };
+
+// export const fetchDashboardExamCleanCount = async () => {
+//   try {
+//     const response = await axios.get(`${API_URL}/getExamClean`);
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error fetching exam total count:", error);
+//     throw new Error("Failed to fetch dashboard information");
+//   }
+// };
+
+// export const fetchDashboardExamIncidentCount = async () => {
+//   try {
+//     const response = await axios.get(
+//       `${API_URL}/getExamIncident?countOnly=true`
+//     );
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error fetching exam total count:", error);
+//     throw new Error("Failed to fetch dashboard information");
+//   }
+// };
